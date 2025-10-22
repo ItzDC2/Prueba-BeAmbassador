7b146061e1a9f4e74caf18921df1a5c1611474bd
Dónovan Castro Fariña <donovancf12380@gmail.com>
2025-10-23 00:29:57 +0100

    Añadido framework Angular Material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Prueba-BeAmbassador');
 }
